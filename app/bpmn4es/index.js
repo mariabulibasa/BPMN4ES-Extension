e66feb739deb3ca2a33db52e27246e2c4561275a
mariabulibasa <maria.bulibasa14@gmail.com>
2025-06-14 19:07:24 +0200

    added KEI condition popup with process variables on sequence flows the go out of XOR gateways

--- app/bpmn4es/index.js
+++ app/bpmn4es/index.js
@@ -2,7 +2,8 @@ import KeiContextPad from './KeiContextPad';
 import KeiRenderer from './KeiRenderer';
 import KeiMenuProvider from './KeiMenuProvider';
 import KeiEventRenderer from './KeiEventRenderer';
-import ClearKEIOnReplace          from './ClearKeiOnReplace';
+import ClearKEIOnReplace from './ClearKeiOnReplace';
+import SequenceFlowContextPad from './SequenceFlowContextPad';
 
 export default {
   __init__: [
@@ -10,11 +11,13 @@ export default {
     'keiRenderer',
     'keiMenuProvider',
     'keiEventRenderer',
-    'clearKeiOnReplace'
+    'clearKeiOnReplace',
+    'sequenceFlowContextPad'
   ],
   keiContextPad:                        ['type', KeiContextPad],
   keiRenderer:                         ['type', KeiRenderer],
   keiMenuProvider:                     ['type', KeiMenuProvider],
   keiEventRenderer:                     ['type', KeiEventRenderer],
   clearKeiOnReplace:              ['type', ClearKEIOnReplace],
+  sequenceFlowContextPad:     ['type', SequenceFlowContextPad]
 };
